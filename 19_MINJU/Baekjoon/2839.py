e1c669348c24b1908dbc7aaf22549a52874d00c4
minju123486 <minju123486@gmail.com>
2024-12-11 00:48:35 +0900

    2839 update

---
+++ 19_MINJU/Baekjoon/2839.py
@@ -0,0 +1,26 @@
+n = int(input())
+cnt = 0
+if(n < 5):
+    n -= 3
+    cnt += 1
+if(n >= 5):
+    if(n % 5 == 0):
+        cnt = n // 5
+        n = n % 5
+    elif(n % 3 == 0):
+        while(n > 0):
+            n -= 3
+            cnt += 1
+            if(n % 5 == 0):
+                cnt += (n // 5)
+                n = n % 5
+    else:
+        while(n > 0):
+            n -= 3
+            cnt += 1
+            if(n % 5 == 0):
+                cnt += n // 5
+                n = n % 5
+if n != 0:
+    cnt = -1
+print(cnt)
